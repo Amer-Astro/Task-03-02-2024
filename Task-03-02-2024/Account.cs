ada70c20b98c92a8b83a22d5c6e9e67d785cfd0c
Astro-PC <amerxmostafa@gmail.com>
2025-02-08 21:00:54 +0200

    Add project files.

---
+++ Task-03-02-2024/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_02_2024
+{
+    internal class Account
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public Account(string Name = "Unnamed Account", double Balance = 0.0)
+        {
+            this.Name = Name;
+            this.Balance = Balance;
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public static implicit operator Account(List<SavingsAccount> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
