cf372e7a7f6e49c2b8766af2e764ce3d3e614a78
Astro-PC <amerxmostafa@gmail.com>
2025-02-12 21:16:18 +0200

    Exception handling 1.0

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Enter Array lenth: ");
+            int arrayLingh = int.Parse(Console.ReadLine());
+
+            int[] array = new int[arrayLingh];
+
+            for (int i = 0; i < arrayLingh; i++) 
+            {
+                Console.Write($"Enter index number {i+1}: ");
+                int index = int.Parse(Console.ReadLine());
+
+                if (array.Contains(index))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new Exception("This index already exist");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                array[i] = index;
+            }
+            foreach (var Arr in array)
+            {
+                Console.WriteLine(Arr);
+            }
+        }
+    }
+}
