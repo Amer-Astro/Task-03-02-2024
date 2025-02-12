bd992b3678d0fc62f9e3c7a92cfd3ac010579271
Astro-PC <amerxmostafa@gmail.com>
2025-02-12 21:32:11 +0200

    Exeption2

--- Exception Handling/Program.cs
+++ Exception Handling/Program.cs
@@ -1,17 +1,28 @@
-﻿namespace Exception_Handling
+﻿using System.Linq;
+
+namespace Exception_Handling
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+
+            //Exeption1();
+
+            //Exeption2();
+
+        }
+
+        static void Exeption1()
         {
             Console.Write($"Enter Array lenth: ");
             int arrayLingh = int.Parse(Console.ReadLine());
 
             int[] array = new int[arrayLingh];
 
-            for (int i = 0; i < arrayLingh; i++) 
+            for (int i = 0; i < arrayLingh; i++)
             {
-                Console.Write($"Enter index number {i+1}: ");
+                Console.Write($"Enter index number {i + 1}: ");
                 int index = int.Parse(Console.ReadLine());
 
                 if (array.Contains(index))
@@ -27,6 +38,27 @@
             {
                 Console.WriteLine(Arr);
             }
+        }
+
+        static void Exeption2()
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            Console.Write("Enter the string: ");
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; input.Length >= i; i++) 
+            {
+                if (vowels.Contains(input[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    throw new Exception("This Input has Vowels");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+
+            Console.WriteLine($"input is {input} and it has no Vowels on it");
         }
     }
 }
